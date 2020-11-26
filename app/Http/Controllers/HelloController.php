8eea2f0a94569be09f491579ababb55a9b25eb2e
yoon8303 <m_maehara@ysmwork.com>
2020-11-27 08:56:49 +0900

    初回コミット

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\HelloRequest;
+use Illuminate\Http\Response;
+use Validator;
+use Illuminate\Support\Facades\DB;
+
+class HelloController extends Controller
+{
+    public function index(Request $request)
+    {
+       $items = DB::table('people')->orderBy('age', 'desc')->get();
+       return view('hello.index', ['items' => $items]);
+    }
+
+    public function show(Request $request)
+    {
+       $page = $request->page;
+       $items = DB::table('people')
+           ->offset($page * 3)
+           ->limit(3)
+           ->get();
+       return view('hello.show', ['items' => $items]);
+    }
+    
+    public function post(Request $request)
+    {
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
+    }
+
+    public function add(Request $request)
+    {
+       return view('hello.add');
+    }
+    
+    public function create(Request $request)
+    {
+       $param = [
+           'name' => $request->name,
+           'mail' => $request->mail,
+           'age' => $request->age,
+       ];
+       DB::table('people')->insert($param);
+       return redirect('/hello');
+    }
+
+    public function edit(Request $request)
+    {
+       $item = DB::table('people')
+           ->where('id', $request->id)->first();
+       return view('hello.edit', ['form' => $item]);
+    }
+    
+    public function update(Request $request)
+    {
+       $param = [
+           'name' => $request->name,
+           'mail' => $request->mail,
+           'age' => $request->age,
+       ];
+       DB::table('people')
+           ->where('id', $request->id)
+           ->update($param);
+       return redirect('/hello');
+    }
+
+    public function del(Request $request)
+    {
+       $item = DB::table('people')
+           ->where('id', $request->id)->first();
+       return view('hello.del', ['form' => $item]);
+    }
+    
+    public function remove(Request $request)
+    {
+       DB::table('people')
+           ->where('id', $request->id)->delete();
+       return redirect('/hello');
+    }
+}
---
+++ app/Http/Requests/HelloRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class HelloRequest extends FormRequest
+{
+   public function authorize()
+   {
+       if ($this->path() ==  'hello')
+       {
+           return true;
+       } else {
+           return false;
+       }
+   }
+
+   public function rules()
+   {
+       return [
+           'name' => 'required',
+           'mail' => 'email',
+           'age' => 'numeric|hello',
+       ];
+   }
+
+   public function messages()
+   {
+       return [
+           'name.required' => '名前は必ず入力して下さい。',
+           'mail.email'  => 'メールアドレスが必要です。',
+           'age.numeric' => '年齢を整数で記入下さい。',
+           'age.hello' => 'Hello! 入力は偶数のみ受け付けます。',
+       ];
+   }
+
+}
