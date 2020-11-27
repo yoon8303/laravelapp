c6d42977d14c96c290511eb4bd119c6244d19577
yoon8303 <m_maehara@ysmwork.com>
2020-11-27 14:24:19 +0900

    6-13

--- app/person.php
+++ app/person.php
@@ -14,4 +14,13 @@ class person extends Model
     {
         return $query->where('name', $str);
     }
+    public function scopeAgeGreaterThan($query, $n)
+    {
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n)
+    {
+        return $query->where('age', '<=', $n);
+    }
 }
