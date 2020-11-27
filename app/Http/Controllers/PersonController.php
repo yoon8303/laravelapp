b77eeddf8aff3fe5f8c4574312c1d243e6583078
yoon8303 <m_maehara@ysmwork.com>
2020-11-27 11:26:14 +0900

    6-5

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -12,8 +12,5 @@ class PersonController extends Controller
        $items = Person::all();
        return view('person.index', ['items' => $items]);
    }
-   public function getData()
-{
-   return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
-}
+
 }
--- app/person.php
+++ app/person.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class person extends Model
 {
-    //
+    public function getData()
+{
+   return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+}
 }
