4b5d3edb9e3811341c2f95fc70fd83369e9b4f81
yoon8303 <m_maehara@ysmwork.com>
2020-11-27 11:19:59 +0900

    6-6

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -9,12 +9,10 @@
 
 @section('content')
    <table>
-   <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+   <tr><th>Data</th></tr>
    @foreach ($items as $item)
        <tr>
-           <td>{{$item->name}}</td>
-           <td>{{$item->mail}}</td>
-           <td>{{$item->age}}</td>
+           <td>{{$item->getData()}}</td>
        </tr>
    @endforeach
    </table>
