ae87e4efe0fc42c09beeabaa02c14fbf6dc27267
yoon8303 <m_maehara@ysmwork.com>
2020-11-27 16:37:52 +0900

    6-21

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,5 @@ Route::get('person', 'PersonController@index');
 Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
 
-
+Route::get('person/add', 'PersonController@add');
+Route::post('person/add', 'PersonController@create');
