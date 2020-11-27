00594d836c183a65f2b010ab062e3729cad93a6a
yoon8303 <m_maehara@ysmwork.com>
2020-11-27 12:58:16 +0900

    6-12

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -14,7 +14,7 @@ class PersonController extends Controller
     
     public function search(Request $request)
     {
-       $item = Person::where('name', $request->input)->first();
+       $item = Person::nameEqual($request->input)->first();
        $param = ['input' => $request->input, 'item' => $item];
        return view('person.find', $param);
     }
