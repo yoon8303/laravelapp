0c5a73893770077fb78481076269d45961e333e0
yoon8303 <m_maehara@ysmwork.com>
2020-11-27 11:42:54 +0900

    6-7

--- resources/views/person/find.blade.php
+++ resources/views/person/find.blade.php
@@ -8,7 +8,7 @@
 @endsection
 
 @section('content')
-   <form action="/person/find" method="post">
+   <form action="../person/find" method="post">
    {{ csrf_field() }}
    <input type="text" name="input" value="{{$input}}">
    <input type="submit" value="find">
