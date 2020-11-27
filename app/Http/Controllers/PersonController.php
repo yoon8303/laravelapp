6f5120d5c1063080f042fdceb0e272285018af62
yoon8303 <m_maehara@ysmwork.com>
2020-11-27 11:34:01 +0900

    6-8

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -7,10 +7,16 @@ use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
-   public function index(Request $request)
-   {
-       $items = Person::all();
-       return view('person.index', ['items' => $items]);
-   }
+    public function find(Request $request)
+    {
+       return view('person.find',['input' => '']);
+    }
+    
+    public function search(Request $request)
+    {
+       $item = Person::find($request->input);
+       $param = ['input' => $request->input, 'item' => $item];
+       return view('person.find', $param);
+    }
 
 }
