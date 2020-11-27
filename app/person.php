c3e77a7337e58f331afbecf29a9b38e5ac4e00bd
yoon8303 <m_maehara@ysmwork.com>
2020-11-27 16:30:20 +0900

    6-18

--- app/person.php
+++ app/person.php
@@ -5,29 +5,20 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\Scopes\ScopePerson;
 
-class person extends Model
+class Person extends Model
 {
-    public function getData()
-    {
-        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
-    }
-    public function scopeNameEqual($query, $str)
-    {
-        return $query->where('name', $str);
-    }
-    public function scopeAgeGreaterThan($query, $n)
-    {
-        return $query->where('age', '>=', $n);
-    }
+   protected $guarded = array('id');
 
-    public function scopeAgeLessThan($query, $n)
-    {
-        return $query->where('age', '<=', $n);
-    }
+   public static $rules = array(
+      'name' => 'required',
+      'mail' => 'email',
+      'age' => 'integer|min:0|max:150'
+   );
 
-    protected static function boot()
-    {
-        parent::boot();
-        static::addGlobalScope(new ScopePerson);
-    }
+   // getDataは残しておく
+   public function getData()
+   {
+      return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+   }
 }
+
