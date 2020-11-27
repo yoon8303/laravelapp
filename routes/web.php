5d6a3cafa2f2c6bddc877771d5e6eca7249e4b86
yoon8303 <m_maehara@ysmwork.com>
2020-11-27 11:15:03 +0900

    6-4

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,6 @@ Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
 
 Route::get('hello/show', 'HelloController@show');
+Route::get('person', 'PersonController@index');
 
 
