ceef869902a2133fd317018f2b45a98106dee672
maehara <m_maehara@ysmwork.com>
2020-11-28 22:09:33 +0900

    Person del

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,6 @@ Route::post('person/add', 'PersonController@create');
 
 Route::get('person/edit', 'PersonController@edit');
 Route::post('person/edit', 'PersonController@update');
+
+Route::get('person/del', 'PersonController@delete');
+Route::post('person/del', 'PersonController@remove');
