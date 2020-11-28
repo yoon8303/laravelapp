3a6d5923255309e445096deb6350e1822a9deec1
maehara <m_maehara@ysmwork.com>
2020-11-28 22:02:46 +0900

    Old Person →Person

--- app/Person.php
+++ app/Person.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
-    //
+   protected $guarded = array('id');
+
+   public static $rules = array(
+      'name' => 'required',
+      'mail' => 'email',
+      'age' => 'integer|min:0|max:150'
+   );
+
+   // getDataは残しておく
+   public function getData()
+   {
+      return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+   }
 }
