c8bdba7418f281207c0ee82264c06fbdf4c3f932
maehara <m_maehara@ysmwork.com>
2020-11-28 22:23:59 +0900

    6-30

--- app/Board.php
+++ app/Board.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Board extends Model
 {
-    //
+
+   protected $guarded = array('id');
+  
+    public static $rules = array(
+       'person_id' => 'required',
+       'title' => 'required',
+       'message' => 'required'
+   );
+
+   public function getData()
+   {
+       return $this->id . ': ' . $this->title;
+   }
+
 }
+
