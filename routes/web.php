75023be5479f2c40c72ea70911f04f06d6e965f5
maehara <m_maehara@ysmwork.com>
2020-11-29 18:18:05 +0900

    6-34

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,8 @@ Route::post('person/edit', 'PersonController@update');
 
 Route::get('person/del', 'PersonController@delete');
 Route::post('person/del', 'PersonController@remove');
+
+Route::get('board', 'BoardController@index');
+
+Route::get('board/add', 'BoardController@add');
+Route::post('board/add', 'BoardController@create');
