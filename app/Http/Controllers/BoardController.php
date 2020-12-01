acb243f5a817bb6a3ef57b07779e7d9231b659e4
yoon8303 <m_maehara@ysmwork.com>
2020-12-01 15:19:19 +0900

    6-42

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -8,11 +8,11 @@ use Illuminate\Http\Request;
 class BoardController extends Controller
 {
 
-   public function index(Request $request)
-   {
-       $items = Board::all();
+    public function index(Request $request)
+    {
+       $items = Board::with('person')->get();
        return view('board.index', ['items' => $items]);
-   }
+    }
 
    public function add(Request $request)
    {
