4a60cd34990ddb9e2138a14219e0e940615819d3
yoon8303 <m_maehara@ysmwork.com>
2020-12-01 15:13:56 +0900

    6-39

--- app/Board.php
+++ app/Board.php
@@ -15,10 +15,18 @@ class Board extends Model
        'message' => 'required'
    );
 
-   public function getData()
-   {
-       return $this->id . ': ' . $this->title;
-   }
+// 新たにメソッドを追加
+public function person()
+{
+   return $this->belongsTo('App\Person');
+}
+
+// 既にあるメソッドを修正
+public function getData()
+{
+   return $this->id . ': ' . $this->title . ' (' 
+      . $this->person->name . ')';
+}
 
 }
 
