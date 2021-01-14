db33c408fb5346655e9678ced40d7261f98e9b14
yoon8303 <m_maehara@ysmwork.com>
2021-01-14 15:48:35 +0900

    7-32

--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,6 @@ Route::post('hello/session', 'HelloController@ses_put');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('hello/auth', 'HelloController@getAuth');
+Route::post('hello/auth', 'HelloController@postAuth');
