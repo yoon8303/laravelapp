c8fef3121cadca845eb862b3cd13cd8cd1e4458d
yoon8303 <m_maehara@ysmwork.com>
2021-01-14 13:28:51 +0900

    7-21

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,3 +1,4 @@
+/*
 @extends('layouts.helloapp')
 
 @section('title', 'Index')
@@ -22,4 +23,34 @@
 
 @section('footer')
 copyright 2020 minmin.
+@endsection
+*/
+@extends('layouts.helloapp')
+<style>
+   .pagination { font-size:10pt; }
+   .pagination li { display:inline-block }
+</style>
+@section('title', 'Index')
+
+@section('menubar')
+   @parent
+   インデックスページ
+@endsection
+
+@section('content')
+   <table>
+   <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+   @foreach ($items as $item)
+       <tr>
+           <td>{{$item->name}}</td>
+           <td>{{$item->mail}}</td>
+           <td>{{$item->age}}</td>
+       </tr>
+   @endforeach
+   </table>
+   {{ $items->links() }}
+@endsection
+
+@section('footer')
+copyright 2017 tuyano.
 @endsection
