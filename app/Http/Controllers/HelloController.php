bc9e391f68c425666ecff38e264426ab5584b2c9
yoon8303 <m_maehara@ysmwork.com>
2021-01-14 14:01:50 +0900

    7-24

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -15,7 +15,7 @@ class HelloController extends Controller
     {
       $sort = $request->sort;
       $items = Person::orderBy($sort, 'asc')
-         ->simplePaginate(5);
+         ->paginate(5);
       $param = ['items' => $items, 'sort' => $sort];
       return view('hello.index', $param);
 
