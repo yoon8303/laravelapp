82590f0a05f9770362edfff4beb35a76d71996f8
yoon8303 <m_maehara@ysmwork.com>
2021-01-14 11:08:41 +0900

    7-6

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,5 @@ Route::get('board', 'BoardController@index');
 
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
+
+Route::resource('rest', 'RestappController');
