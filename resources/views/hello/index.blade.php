277003e00d33abf625d8ae5c649faa012308562d
yoon8303 <m_maehara@ysmwork.com>
2021-01-14 13:31:53 +0900

    7-21-1

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,30 +1,3 @@
-/*
-@extends('layouts.helloapp')
-
-@section('title', 'Index')
-
-@section('menubar')
-   @parent
-   インデックスページ
-@endsection
-
-@section('content')
-   <table>
-   <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
-   @foreach ($items as $item)
-       <tr>
-           <td>{{$item->name}}</td>
-           <td>{{$item->mail}}</td>
-           <td>{{$item->age}}</td>
-       </tr>
-   @endforeach
-   </table>
-@endsection
-
-@section('footer')
-copyright 2020 minmin.
-@endsection
-*/
 @extends('layouts.helloapp')
 <style>
    .pagination { font-size:10pt; }
