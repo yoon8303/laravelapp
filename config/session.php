edd970557f3d3faee9972369d11330bd86928a8c
yoon8303 <m_maehara@ysmwork.com>
2021-01-14 12:44:42 +0900

    7-17

--- config/session.php
+++ config/session.php
@@ -17,8 +17,9 @@ return [
     |            "memcached", "redis", "dynamodb", "array"
     |
     */
+    'driver' => env('SESSION_DRIVER', 'database'),
 
-    'driver' => env('SESSION_DRIVER', 'file'),
+    //'driver' => env('SESSION_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
