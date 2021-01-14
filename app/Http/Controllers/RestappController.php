a8dc7566522b1c7f775c9bfd740df2e9d9ffeff2
yoon8303 <m_maehara@ysmwork.com>
2021-01-14 11:30:52 +0900

    7-9

--- app/Http/Controllers/RestappController.php
+++ app/Http/Controllers/RestappController.php
@@ -27,6 +27,7 @@ class RestappController extends Controller
     public function create()
     {
         //
+        return view('rest.create');
     }
 
     /**
@@ -38,6 +39,11 @@ class RestappController extends Controller
     public function store(Request $request)
     {
         //
+        $restdata = new Restdata;
+        $form = $request->all();
+        unset($form['_token']);
+        $restdata->fill($form)->save();
+        return redirect('/rest');
     }
 
     /**
