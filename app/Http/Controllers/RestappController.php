dd390f8a5b3c7f4a69547268214cd2d8f4b1d42c
yoon8303 <m_maehara@ysmwork.com>
2021-01-14 11:13:02 +0900

    7-6

--- app/Http/Controllers/RestappController.php
+++ app/Http/Controllers/RestappController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Restdata;
 
 class RestappController extends Controller
 {
@@ -14,6 +15,8 @@ class RestappController extends Controller
     public function index()
     {
         //
+        $items = Restdata::all();
+        return $items->toArray();
     }
 
     /**
@@ -46,6 +49,8 @@ class RestappController extends Controller
     public function show($id)
     {
         //
+        $item = Restdata::find($id);
+        return $item->toArray();
     }
 
     /**
